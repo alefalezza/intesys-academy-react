7a42b89409fa7cf1e86c0f1978eafcc39e2ae4be
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-05-19 19:18:30 +0200

    material theme setup

--- src/components/layout/body/index.tsx
+++ src/components/layout/body/index.tsx
@@ -1,11 +1,17 @@
 import { Cell, Row } from "@material/react-layout-grid";
+import TextField, { Input } from "@material/react-text-field";
 import React from "react";
 import "./index.scss";
 
 const Body: React.FunctionComponent = props => (
   <div className='layout-body'>
     <Row>
-      <Cell>{props.children}</Cell>
+      <Cell>
+        {props.children}
+        <TextField label='Name'>
+          <Input value='' />
+        </TextField>
+      </Cell>
     </Row>
   </div>
 );
