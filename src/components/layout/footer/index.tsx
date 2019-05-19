a72ef64deef45dc2faaade1e7dc40950a5aab6b0
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-05-19 21:47:09 +0200

    added logo informatici

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,7 @@
 import { Cell, Row } from "@material/react-layout-grid";
 import React from "react";
 import "./index.scss";
+import informatici from "../../../assets/informatici_senza_frontiere.png";
 
 const Footer: React.FunctionComponent = () => (
   <div className='layout-footer'>
@@ -12,6 +13,9 @@ const Footer: React.FunctionComponent = () => (
               Open Hospital è un progetto di ISF 2005 - 2016 ISF © Informatici
               senza frontiere - ONLUS Viale IV Novembre, 100 - 31100 Treviso -
               Italy C.F. 94106980264
+            </Cell>
+            <Cell columns={8} className='layout-footer__logo-informatici'>
+              <img src={informatici} />
             </Cell>
           </Row>
         </div>
