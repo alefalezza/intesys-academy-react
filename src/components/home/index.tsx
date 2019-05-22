f684c4ff2e2a850b13dbe9cbd5fe34204b50e16f
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-05-22 22:47:42 +0200

    added running out drug list

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,10 +1,14 @@
+import Button from "@material/react-button";
+import Card from "@material/react-card";
+import { Cell, Row } from "@material/react-layout-grid";
+import MaterialIcon from "@material/react-material-icon";
+import Tab from "@material/react-tab";
+import TabBar from "@material/react-tab-bar";
+import { Headline6, Overline } from "@material/react-typography";
 import React from "react";
 import Breadcrumb from "../breadcrumb/breadcrumb";
 import BreadcrumbEl from "../breadcrumb/breadcrumb-el";
-import { Row, Cell } from "@material/react-layout-grid";
-import MaterialIcon from "@material/react-material-icon";
 import "./index.scss";
-import Button from "@material/react-button";
 
 const Home: React.FunctionComponent = () => {
   return (
@@ -12,7 +16,8 @@ const Home: React.FunctionComponent = () => {
       <Breadcrumb>
         <BreadcrumbEl>Home</BreadcrumbEl>
       </Breadcrumb>
-      <Row>
+
+      <Row className="home__row home__call_to_actions">
         <Cell columns={6} className="home__main_button_cell--right">
           <Button className="home__main_button home__main_button--active">
             <MaterialIcon icon="add" />
@@ -24,6 +29,95 @@ const Home: React.FunctionComponent = () => {
             <MaterialIcon icon="search" />
             Search for patients
           </Button>
+        </Cell>
+      </Row>
+
+      <Row className="home__row home__main">
+        <Cell columns={6}>
+          <Overline>Materials are running out</Overline>
+          <Card>
+            <TabBar className="drug_list__header">
+              <Tab active>Running out drugs</Tab>
+              <Tab>Running out nursing material</Tab>
+            </TabBar>
+            <section className="drug_list__item">
+              <Row>
+                <Cell columns={12}>
+                  <h3>Erotricimina</h3>
+                </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">150</div>
+                  <div className="drug_list__item__caption">
+                    Vials remaining
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">4</div>
+                  <div className="drug_list__item__caption">
+                    weeks for the next refueling
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <Button raised>Go to details</Button>
+                </Cell>
+              </Row>
+            </section>
+            <section className="drug_list__item">
+              <Row>
+                <Cell columns={12}>
+                  <h3>Erotricimina</h3>
+                </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">150</div>
+                  <div className="drug_list__item__caption">
+                    Vials remaining
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">4</div>
+                  <div className="drug_list__item__caption">
+                    weeks for the next refueling
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <Button raised>Go to details</Button>
+                </Cell>
+              </Row>
+            </section>
+            <section className="drug_list__item">
+              <Row>
+                <Cell columns={12}>
+                  <h3>Erotricimina</h3>
+                </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">150</div>
+                  <div className="drug_list__item__caption">
+                    Vials remaining
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <div className="drug_list__item__quantity">4</div>
+                  <div className="drug_list__item__caption">
+                    weeks for the next refueling
+                  </div>
+                </Cell>
+                <Cell columns={4} align="middle" className="cell--center">
+                  <Button raised>Go to details</Button>
+                </Cell>
+              </Row>
+            </section>
+            <Tab className="drug_list__see_all_button">See all materials</Tab>
+          </Card>
+        </Cell>
+        <Cell columns={6}>
+          <Overline>Calendar</Overline>
+          <Card>content</Card>
         </Cell>
       </Row>
     </div>
