b6d61dbf8f1ff89b7ae3dd90d5e18f59bb326b8e
Alessandro Falezza <alessandro.falezza@intesys.it>
2019-06-07 17:31:08 +0200

    Use effect in Home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,16 +1,30 @@
 import Card from "@material/react-card";
 import { Cell, Row } from "@material/react-layout-grid";
 import { Overline } from "@material/react-typography";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Breadcrumb from "../breadcrumb/breadcrumb";
 import BreadcrumbEl from "../breadcrumb/breadcrumb-el";
 import Calendar from "./calendar";
 import CallToActions from "./call_to_actions";
 import "./index.scss";
-import { materialList } from "./materials/data"; // tmp: data will be fetched from api
 import MaterialList from "./materials/material_list";
+import { IMaterialList } from "./materials/types";
+
+const apiEndpoint = "http://localhost:3000/materials";
+
+const getMaterials = (): Promise<IMaterialList> =>
+  fetch(apiEndpoint).then(r => r.json());
 
 const Home: React.FunctionComponent = () => {
+  const [materials, setMaterials] = useState({
+    drugs: [],
+    nursing: []
+  } as IMaterialList);
+
+  useEffect(() => {
+    getMaterials().then(materials => setMaterials(materials));
+  }, []);
+
   return (
     <div className="home">
       <Breadcrumb>
@@ -23,7 +37,7 @@ const Home: React.FunctionComponent = () => {
         <Cell columns={6}>
           <Overline>Materials are running out</Overline>
           <Card>
-            <MaterialList {...materialList} />
+            <MaterialList {...materials} />
           </Card>
         </Cell>
         <Cell columns={6}>
